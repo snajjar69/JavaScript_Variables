0d9aee979c221eab21451a99d5cfa42d17c962dc
snajjar69 <snajjar69@gmail.com>
2024-08-21 16:12:33 -0400

    Completed JavaScript Variable

--- app.js
+++ app.js
@@ -4,7 +4,32 @@ console.log(
 );
 
 // Exercise 1
+let lastname = Rummer;
+var age = 36;
+
 
 // Exercise 2
+const name = 'JOE';
+let lastname = 'Rummer';
+var age = '55';
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = '1995';
+let isCaseSensitive = 'TRUE';
+
+//Exercise 4
+let price = "19.99";
+let isOnSale = "false";
+let SalePercentage = "15";
+let stock = "0";
+let inStock = "false";
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the wind";
+let author = "Patrick Rothfuss";
+let pageCount = "722";
+let bookmark = "456";
+let hasRead = "true";
+
